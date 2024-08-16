6668ed05d73c2b8460a0fba0aca612cd316b69a1
Munck <flavioives23@gmail.com>
2024-08-16 12:57:21 -0300

    Atualizar o arquivo main.py

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import yfinance as yf
 
 #criar funcoes
 @st.cache_data
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(empresa)
+def carregar_dados(empresas):
+    texto_tickers = " ".join(empresas)
+    dados_acao = yf.Tickers(empresas)
     cotacoes_acao = dados_acao.history(period="1d", start="2010-01-01", end="2024-08-01")
-    cotacoes_acao = cotacoes_acao[["Close"]]
+    cotacoes_acao = cotacoes_acao["Close"]
     return cotacoes_acao
 
+acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "BBDC4.SA", "LREN3.SA", "BBAS3.SA"]
+dados = carregar_dados(acoes)
+
 #preparar visualizacoes
-dados = carregar_dados("PETR4.SA")
+lista_acoes = st.multiselect("Escolha as ações:", dados.columns)
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
+    
+
 #criar a interface
 st.write("""# Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações da Petrobrás(PETR4) ao longo dos anos""") #markdown
+O gráfico abaixo representa a evolução do preço das ações ao longo dos anos""") #markdown
 
 st.line_chart(dados) #grafico de linha
